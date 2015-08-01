41149bbd8a0950696db542880cb77eb5704ea530
Timandes White <timands@gmail.com>
2015-08-01 21:02:45 +0800

    display version info.

--- libzookeeper.c
+++ libzookeeper.c
@@ -9,6 +9,7 @@
 #include "ext/standard/info.h"
 #include "php_libzookeeper.h"
 #include "zookeeper_client.h"
+#include "version.h"
 
 /* If you declare any globals in php_libzookeeper.h uncomment this:
 ZEND_DECLARE_MODULE_GLOBALS(libzookeeper)
@@ -41,7 +42,7 @@ zend_module_entry libzookeeper_module_entry = {
     PHP_RSHUTDOWN(libzookeeper),    /* Replace with NULL if there's nothing to do at request end */
     PHP_MINFO(libzookeeper),
 #if ZEND_MODULE_API_NO >= 20010901
-    "0.1", /* Replace with version number for your extension */
+    LIBZOOKEEPER_VERSION, /* Replace with version number for your extension */
 #endif
     STANDARD_MODULE_PROPERTIES
 };
@@ -76,7 +77,7 @@ static void php_libzookeeper_init_globals(zend_libzookeeper_globals *libzookeepe
  */
 PHP_MINIT_FUNCTION(libzookeeper)
 {
-    /* If you have INI entries, uncomment these lines 
+    /* If you have INI entries, uncomment these lines
     REGISTER_INI_ENTRIES();
     */
 
@@ -123,8 +124,16 @@ PHP_RSHUTDOWN_FUNCTION(libzookeeper)
  */
 PHP_MINFO_FUNCTION(libzookeeper)
 {
+    char version[10];
+
     php_info_print_table_start();
     php_info_print_table_header(2, "libzookeeper support", "enabled");
+
+    php_info_print_table_row(2, "Version", LIBZOOKEEPER_VERSION);
+
+    snprintf(version, sizeof(version), "%d.%d.%d", ZOO_MAJOR_VERSION, ZOO_MINOR_VERSION, ZOO_PATCH_VERSION);
+	php_info_print_table_row(2, "Zookeeper C Client Version", version);
+
     php_info_print_table_end();
 
     /* Remove comments if you have entries in php.ini
@@ -155,9 +164,9 @@ PHP_FUNCTION(confirm_libzookeeper_compiled)
     RETURN_STRINGL(strg, len, 0);
 }
 /* }}} */
-/* The previous line is meant for vim and emacs, so it can correctly fold and 
-   unfold functions in source code. See the corresponding marks just before 
-   function definition, where the functions purpose is also documented. Please 
+/* The previous line is meant for vim and emacs, so it can correctly fold and
+   unfold functions in source code. See the corresponding marks just before
+   function definition, where the functions purpose is also documented. Please
    follow this convention for the convenience of others editing your code.
 */
 
---
+++ version.h
@@ -0,0 +1,21 @@
+/* $Id$ */
+
+#ifndef LIBZOOKEEPER_VERSION_H
+#define LIBZOOKEEPER_VERSION_H
+
+#define LIBZOOKEEPER_MAJOR_VERSION 0
+#define LIBZOOKEEPER_MINOR_VERSION 1
+#define LIBZOOKEEPER_PATCH_VERSION 0
+
+#define LIBZOOKEEPER_VERSION "0.1.0"
+
+#endif  /* LIBZOOKEEPER_VERSION_H */
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * End:
+ * vim600: noet sw=4 ts=4 fdm=marker
+ * vim<600: noet sw=4 ts=4
+ */
