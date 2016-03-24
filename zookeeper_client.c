faa5de393cab67412ae1337e80519aa4d67cff76
Timandes White <timands@gmail.com>
2016-03-24 15:38:35 +0800

    fix length of array key

--- zookeeper_client.c
+++ zookeeper_client.c
@@ -810,10 +810,15 @@ void zookeeper_client_acl_vector_2_zarrval(struct ACL_vector *acls, zval *return
     array_init(return_value);
     for (i=0; i<acls->count; ++i) {
         key_len = strlen(acls->data[i].id.id) + strlen(acls->data[i].id.scheme) + 1;
-        key = (char *)calloc(key_len, sizeof(char));
+        key = (char *)calloc(key_len + 1, sizeof(char));
         sprintf(key, "%s:%s", acls->data[i].id.scheme, acls->data[i].id.id);
+        key[key_len] = '\0';
 
-        add_assoc_long_ex(return_value, key, key_len, acls->data[i].perms);
+        add_assoc_long_ex(return_value, key, key_len
+#ifndef ZEND_ENGINE_3
+            + 1
+#endif
+            , acls->data[i].perms);
 
         free(key);
     }
